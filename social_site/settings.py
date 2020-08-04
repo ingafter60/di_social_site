faec0c3ad9f795f5797da6dfd5d80464596c6f61
ing <inyoman_gurnitha@yahoo.com>
2020-08-04 08:07:13 +0700

    3. Template of authentication

--- social_site/settings.py
+++ social_site/settings.py
@@ -121,8 +121,12 @@ USE_L10N = True
 
 USE_TZ = True
 
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+LOGIN_REDIRECT_URL = "/"
+
